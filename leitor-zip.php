d9bd27e30dd9d54ba9e528520c0fc6a622af9d50
Eric Araújo Lima <eric.araujolima@hotmail.com>
2024-03-02 13:57:03 -0300

    Utilizando contexto em streams

--- leitor-zip.php
+++ leitor-zip.php
@@ -1,3 +1,9 @@
 <?php
 
-echo file_get_contents('zip://arquivos.zip#viagens.txt');
+$contexto = stream_context_create([
+    'zip' => [
+        'password' => '123456'
+    ]
+]);
+
+echo file_get_contents('zip://viagens.zip#viagens.txt', false, $contexto);
